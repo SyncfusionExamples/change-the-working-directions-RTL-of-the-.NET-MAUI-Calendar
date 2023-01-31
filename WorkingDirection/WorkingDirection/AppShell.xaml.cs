75f4bae321c15dadd7fe21aeecadb7ddf28051a0
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-31 23:27:00 +0530

    New KB sample added

---
+++ WorkingDirection/WorkingDirection/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkingDirection;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ WorkingDirection/WorkingDirection/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkingDirection;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
